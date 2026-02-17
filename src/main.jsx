c309275b3df436237b850f8304aa3c0e0e724567
Hans Schubert S. Baron <Cyzer5568@gmail.com>
2026-02-17 16:00:32 +0800

    Troubleshooted tailwind css correctly

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './app.css' // <--- MAKE SURE THIS LINE EXISTS AND MATCHES YOUR FILENAME
-// or import './App.css' if you put the @tailwind stuff there
+import './index.css' // <--- CHANGE THIS (was ./app.css)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
